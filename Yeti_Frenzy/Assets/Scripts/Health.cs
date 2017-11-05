542f320c5b09172a25bd67dd1d00ab7bbcba0188
iirlas <iirlas@yahoo.com>
2017-11-05 01:34:49 -0500

    Added Yeti_Frenzy\Assets\Scripts\Health.cs

---
+++ Yeti_Frenzy/Assets/Scripts/Health.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+	public float amount = 100.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (amount <= 0.0f)
+		{
+			gameObject.SetActive(false);
+		}	
+	}
+
+	void ApplyDamage (float amount)
+	{
+		this.amount -= amount;
+	}
+}
