d82f9c36209256e7e482728709060c3480042262
iirlas <iirlas@yahoo.com>
2017-11-05 01:39:47 -0500

    Removed Yeti_Frenzy\Assets\Scripts\GravityScalar.cs
    Revised Yeti_Frenzy\Assets\Scripts\Jump.cs incorporates GravityScalar

--- Yeti_Frenzy/Assets/Scripts/Jump.cs
+++ Yeti_Frenzy/Assets/Scripts/Jump.cs
@@ -7,13 +7,17 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Jump : MonoBehaviour 
 {	
 	public float force = 10.0f;
+	public float positiveMultiplier = 0.5f;
+	public float negativeMultiplier = 2.0f;
 	[HideInInspector]
 	public new Rigidbody rigidbody;
+	private Vector3 gravity;
 
 	// Use this for initialization
 	void Start ()
 	{
 		rigidbody = GetComponent<Rigidbody>();
+		gravity = Physics.gravity;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +27,20 @@ public class Jump : MonoBehaviour
 		if (is_down && rigidbody.velocity.y == 0.0f)
 		{
 			rigidbody.AddForce(0.0f, force, 0.0f, ForceMode.Impulse);
+		}
+
+		bool is_pressed = CrossPlatformInputManager.GetButton("Action");
+		if (rigidbody.velocity.y > 0 && is_pressed)
+		{
+			Physics.gravity = gravity * positiveMultiplier;
+		}
+		else if (rigidbody.velocity.y < 0)
+		{
+			Physics.gravity = gravity * negativeMultiplier;
+		}
+		else
+		{
+			Physics.gravity = gravity;
 		}
 	}
 }
