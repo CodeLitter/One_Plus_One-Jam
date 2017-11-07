e547560c82928df590d4ca26fb76445e44675569
NicholasGuerra <nicholas.guerra87@gmail.com>
2017-11-07 11:55:18 -0600

    Health bar prefab added
    Added a health bar and a corrisponding script the is used to manipulate
    the bar and it has a bool that is switched when the slider is empty

---
+++ Yeti_Frenzy/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float maxHealth;
+    public float minHealth;
+    public float currentHealth;
+
+    private bool alive;
+
+    private Slider slider;
+
+    public bool Alive
+    {
+        get
+        {
+            return alive;
+        }
+
+        set
+        {
+            alive = value;
+        }
+    }
+    
+    void Start ()
+    {
+        slider = GetComponent<Slider>();
+        currentHealth = slider.value;
+        maxHealth = slider.maxValue;
+        minHealth = slider.minValue;
+
+    }
+	
+	void Update ()
+    {
+        slider.value = currentHealth;
+
+        if (currentHealth == minHealth)
+        {
+            alive = false;
+        }
+        else
+        {
+            alive = true;
+        }
+	}
+
+    public void addDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+        if (currentHealth < minHealth)
+        {
+            currentHealth = minHealth;
+        }
+    }
+
+    public void addHealth(int healthAmount)
+    {
+        currentHealth += healthAmount;
+
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+
+    public float healthAmount()
+    {
+        return currentHealth;
+    }
+}
