7fedd6f3c048ab61687912fdb026f57b25d96fd1
iirlas <iirlas@yahoo.com>
2017-11-08 00:53:17 -0600

    Added Yeti_Frenzy\Assets\Scripts\Target.cs Added Yeti_Frenzy\Assets\Scripts\Generator.cs

---
+++ Yeti_Frenzy/Assets/Scripts/Generator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generator : MonoBehaviour
+{
+	public Target[] originals;
+	public float speed = 10.0f;
+	public Transform target;
+	public float delay = 5.0f;
+	public float interval = 1.0f;
+	private Timer timer = new Timer();
+	// Use this for initialization
+	void Start ()
+	{
+		timer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Time.timeSinceLevelLoad < delay)
+		{
+			return;
+		}
+
+		if (timer.Elapsed > interval)
+		{
+			var original = originals[Random.Range(0, originals.Length)];
+			var instance = Instantiate<Target>(original, transform.position, transform.rotation);
+			instance.speed = speed;
+			instance.target = target;
+			timer.Reset();
+		}
+	}
+}
---
+++ Yeti_Frenzy/Assets/Scripts/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+	public Transform target;
+	public float speed = 10.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+		if (transform.position == target.position)
+		{
+			this.gameObject.SetActive(false);
+		}
+	}
+}
