344b343351108de43cea01dd7080823ff10b88ea
iirlas <iirlas@yahoo.com>
2017-11-09 01:58:42 -0600

    Added Yeti_Frenzy\Assets\Scripts\Transitioner.cs Added Yeti_Frenzy\Assets\Scripts\Projectile.cs

---
+++ Yeti_Frenzy/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+	public float amount = 10.0f;
+	public List<string> tags;
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (tags.Contains(collision.transform.tag))
+		{
+			collision.transform.SendMessage("ApplyDamage", amount);
+		}
+	}
+}
---
+++ Yeti_Frenzy/Assets/Scripts/Transitioner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName="Transitioner")]
+public class Transitioner : ScriptableObject
+{
+	public void LoadScene (int scene_index)
+	{
+		SceneManager.LoadScene(scene_index);
+	}
+
+	public void Reload ()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		LoadScene(scene.buildIndex);
+	}
+
+	public void LoadNext ()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		LoadScene(Mathf.Clamp(scene.buildIndex + 1, 0, SceneManager.sceneCountInBuildSettings));
+	}
+
+	public void LoadPrev ()
+	{
+		Scene scene = SceneManager.GetActiveScene();
+		LoadScene(Mathf.Clamp(scene.buildIndex - 1, 0, SceneManager.sceneCountInBuildSettings));
+	}
+}
