bfa188dabe22f56006e3b020f44a92617f2a3a09
NicholasGuerra <nicholas.guerra87@gmail.com>
2017-11-08 20:55:38 -0600

    added the credits Panel to the main menu
    added the credits Panel to the main menu and a script that lets you
    toggle a panel on and off

---
+++ Yeti_Frenzy/Assets/Scripts/PanelToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelToggle : MonoBehaviour
+{
+    public GameObject panel;
+
+    public void openPanel()
+    {
+        panel.SetActive(true);
+    }
+
+    public void closePanel()
+    {
+        panel.SetActive(false);
+    }
+
+}
