68607c789be4f9b6e680fe2e2550135e25d76580
iirlas <iirlas@yahoo.com>
2017-11-10 04:26:43 -0600

    Added Yeti_Frenzy\Assets\Scripts\HatHandle.cs

---
+++ Yeti_Frenzy/Assets/Scripts/HatHandle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatHandle : MonoBehaviour
+{
+	public List<GameObject> hats;
+	public UnityEngine.Events.UnityEvent complete;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (hats.TrueForAll(hat => !hat.activeSelf))
+		{
+			complete.Invoke();
+		}
+	}
+}
