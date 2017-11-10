33f289df421ea7b9764bdd5ee56244b54e4a46af
iirlas <iirlas@yahoo.com>
2017-11-10 00:18:00 -0600

    Added Yeti_Frenzy\Assets\Scripts\FallRespawn.cs

---
+++ Yeti_Frenzy/Assets/Scripts/FallRespawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallRespawn : MonoBehaviour
+{
+	public Transform target;
+	public float lowBound = -10.0f;
+	private Vector3 returnPosition;
+	// Use this for initialization
+	void Start ()
+	{
+		returnPosition = target.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (target.position.y < lowBound)
+		{
+			target.position = returnPosition;
+		}
+	}
+}
