0def42c767ab8c7d7dc3c4bc63a8612db4efd338
iirlas <iirlas@yahoo.com>
2017-11-10 04:49:27 -0600

    Added Yeti_Frenzy\Assets\Scripts\Respawner.cs

---
+++ Yeti_Frenzy/Assets/Scripts/Respawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawner : MonoBehaviour
+{
+	public Transform target;
+	public Transform respawnPoint;
+
+	public void Respawn ()
+	{
+		target.position = respawnPoint.position;
+	}
+}
